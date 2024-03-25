ea096a442bae5e332528841e242180d36803d6d9
Matimateokol <mateuszkolacz2019@gmail.com>
2024-03-25 02:28:08 +0100

    Fix not working main.py

--- EvolutionaryAlgorithm/main.py
+++ EvolutionaryAlgorithm/main.py
@@ -8,18 +8,19 @@ class EvolutionaryAlgorithm:
     LOWER_BOUND = -100
     UPPER_BOUND = 100
     PARAMS_SIZE = 10
-    Q_x = f13  # goal_function, function which we are going to optimize
 
-    budget = 10000
+    Q_x = f2  # goal_function, function which we are going to optimize
+    budget = 10000 # total budget
     population_size = 50  # mu value
     sigma = 3  # param needed for mutation
     t = 0  # current generation (iteration) counter
     tmax = budget / population_size  # max number of permitted generations (iterations)
 
-    def __init__(self, budget, population_size, sigma):
+    def __init__(self, budget, population_size, sigma, Q_x):
         self.budget = budget
         self.population_size = population_size
         self.sigma = sigma
+        self.Q_x = Q_x
         self.t = 0
         self.tmax = budget / population_size
 
@@ -35,8 +36,8 @@ class EvolutionaryAlgorithm:
         return population
 
     # Assessment function
-    def assess(self, x_vector):
-        return self.Q_x(x_vector)
+    def assess(x_vector):
+        return EvolutionaryAlgorithm.Q_x(x_vector)
 
     # Function that mutates an individual from population
     def mutate(self, child):
@@ -51,42 +52,45 @@ class EvolutionaryAlgorithm:
             tournament_scores = [scores[i] for i in tournament_indices]
             winner_index = tournament_indices[np.argmin(tournament_scores)]
             selected_indices.append(winner_index)
-        return temp_population[selected_indices]
+
+        return [temp_population[i] for i in selected_indices]
 
     def run_evolutionary_algorithm(self):
         self.t = 0
         temp_population = self.initialize_population()
 
         while self.t < self.tmax:
-            scores = np.array([self.assess(ind) for ind in temp_population])
+            # print("Generation {}".format(self.t))
+            scores = np.array([EvolutionaryAlgorithm.assess(ind) for ind in temp_population])
             children = self.tournament_selection(temp_population, scores)
             mutated_children = np.array([self.mutate(child) for child in children])
             temp_population = mutated_children
-            self.t = + 1
+            self.t += 1
         final_population = temp_population
-        final_scores = np.array([self.Q_x(ind) for ind in final_population])
+        final_scores = np.array([EvolutionaryAlgorithm.assess(ind) for ind in final_population])
         best_index = np.argmin(final_scores)
         best_individual = final_population[best_index]
         best_individual_score = final_scores[best_index]
-        return [best_individual, best_individual_score]
+        return best_individual, best_individual_score
 
 
 def main():
+
+    population_sizes = [2, 4, 8, 16, 32, 64, 128]
+
     ### Test 1 ###
-    ea1 = EvolutionaryAlgorithm(10_000, 50, 3)
+    ea1 = EvolutionaryAlgorithm(10_000, 50, 3, f2)
     results = []
-    print(ea1.run_evolutionary_algorithm())
-    # for _ in range(30):
-    #     results.append(ea1.run_evolutionary_algorithm())
-    #results.append(ea1.run_evolutionary_algorithm())
+    for _ in range(30):
+        results.append(ea1.run_evolutionary_algorithm()[1])
 
-    # min = min(results)
-    # max = max(results)
-    # mean = np.mean(results)
-    # std = np.std(results)
+    min_val = round(np.min(results), 2)
+    max_val = round(np.max(results), 2)
+    mean_val = round(np.mean(results), 2)
+    std_val = round(np.std(results), 2)
 
-    # print("Population size: ", ea1.population_size, ", Sigma: ", ea1.sigma)
-    # print("Minimum:", min, ", Maximum:", max, ", Mean:", mean, "Standard Deviation:", std)
+    print("Population size: ", ea1.population_size, ", Sigma: ", ea1.sigma)
+    print("Minimum:", min_val, ", Maximum:", max_val, ", Mean:", mean_val, "Standard Deviation:", std_val)
 
 
 if __name__ == '__main__':
